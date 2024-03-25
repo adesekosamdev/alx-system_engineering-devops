571d04606a3bfcaaa8cf4370f36504d54555ccf1
Pizza Man <adesekosamdev@gmail.com>
2024-03-25 04:46:17 +0000

    add tasks

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#make changes to our configuration file
+file { '/etc/ssh/ssh_config':
+  ensure => present
+}->
+file_line { 'pass no':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication yes',
+}->
+file_line { 'IdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile',
+}
