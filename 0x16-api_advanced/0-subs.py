9d3d5330be7bd26bbac2a6e2f3ad08dbdbaef26a
pizza man <adesekosam.dev@gmail.com>
2024-08-13 15:29:34 -0400

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,6 @@ Function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0
 """
-
 import requests
 
 
