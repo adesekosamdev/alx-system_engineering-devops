36cb2fa53db40682780942cbbf4ee09c3afdb643
pizza man <adesekosam.dev@gmail.com>
2024-08-14 07:58:03 -0400

    changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,16 @@
-#!/usr/bin/python3
 """ given employee ID, returns information about his/her TODO list progress """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     """ main code """
-    r_info = requests.get(
-        "https://jsonplaceholder.typicode.com/todos").json()
-    r_users = requests.get(
-        "https://jsonplaceholder.typicode.com/users").json()
-    r_user_dict = list(
-        filter(
-            lambda elem: elem.get("id") == int(
-                argv[1]),
-            r_users))[0]
-    user = r_user_dict.get('name')
-    r_info_filtered = list(
-        filter(
-            lambda elem: elem.get('userId') == int(
-                argv[1]),
-            r_info))
-    done = list(
-        filter(
-            lambda elem: elem.get('completed'),
-            r_info_filtered))
-    num_task = len(done)
-    total_num_task = len(r_info_filtered)
-    print("Employee {} is done with ".format(user), end='')
-    print('tasks({}/{}):'.format(num_task, total_num_task))
-    for i in done:
+    url = "https://jsonplaceholder.typicode.com/"
+    user_response = requests.get(url+'users/'+argv[1]).json()
+    todos_response = requests.get(url+'todos?userId='+argv[1]).json()
+    completed_todos = requests.get(url+'todos?userId='+argv[1]+'&completed=true').json()
+    num_of_done = len(completed_todos)
+    total_num = len(todos_response)
+    user_name = user_response.get("name")
+    print("Employee {} is done with tasks({}/{}):".format(user_name, num_of_done, total_num))
+    for i in completed_todos:
         print('\t {}'.format(i.get('title')))
