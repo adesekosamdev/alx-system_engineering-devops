52f30129e4ebe2f12a93d77fc5bb48d910d357dd
pizza man <adesekosam.dev@gmail.com>
2024-08-14 10:10:49 -0400

    hh

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """ given employee ID, returns information about his/her TODO list progress """
 import requests
 from sys import argv
@@ -8,12 +7,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_response = requests.get(url+'users/'+argv[1]).json()
     todos_response = requests.get(url+'todos?userId='+argv[1]).json()
-    completed_todos = requests.get(url+'todos?userId='+argv[1] + 
+    completed_todos = requests.get(url+'todos?userId='+argv[1] +
                                    '&completed=true').json()
     num_of_done = len(completed_todos)
     total_num = len(todos_response)
     user_name = user_response.get("name")
     print("Employee {} is done with tasks({}/{}):"
-    .format(user_name, num_of_done, total_num))
+          .format(user_name, num_of_done, total_num))
     for i in completed_todos:
         print('\t {}'.format(i.get('title')))
