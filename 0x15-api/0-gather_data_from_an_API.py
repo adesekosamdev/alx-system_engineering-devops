90387b96d04edbbaa9cb6854062dd36234cc73ad
pizza man <adesekosam.dev@gmail.com>
2024-08-14 08:00:24 -0400

    changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,10 +8,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_response = requests.get(url+'users/'+argv[1]).json()
     todos_response = requests.get(url+'todos?userId='+argv[1]).json()
-    completed_todos = requests.get(url+'todos?userId='+argv[1]+'&completed=true').json()
+    completed_todos = requests.get(url+'todos?userId='+argv[1]+
+                                   '&completed=true').json()
     num_of_done = len(completed_todos)
     total_num = len(todos_response)
     user_name = user_response.get("name")
-    print("Employee {} is done with tasks({}/{}):".format(user_name, num_of_done, total_num))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, num_of_done, total_num))
     for i in completed_todos:
         print('\t {}'.format(i.get('title')))
